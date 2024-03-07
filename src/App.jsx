53cb2bcf942b8400aaf846f10f078222f156e5d0
Rafaqat Ali <rafaqatmehar007@gmail.com>
2024-03-07 17:42:52 +0500

    switcher complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { ThemeProvider } from "./contexts/theme";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light")
+
+  // make the method of same it automaticaly get the context
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+
+  // change in theme
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+
   return (
     <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
-
-          <div className="w-full max-w-sm mx-auto"></div>
+              <ThemeBtn />
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
         </div>
       </div>
     </ThemeProvider>
