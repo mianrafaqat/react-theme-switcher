a32952f39e327f8df01154b9506056679038fda8
Rafaqat Ali <rafaqatmehar007@gmail.com>
2024-03-07 16:41:39 +0500

    context setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { ThemeProvider } from "./contexts/theme";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <h1>Theme switcher react</h1>
-    </>
-  )
+    <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+
+          <div className="w-full max-w-sm mx-auto"></div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
